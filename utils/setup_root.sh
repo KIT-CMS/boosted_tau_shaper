b50f98bda41bc6b170f9f7da1a9811598861aadb
Olha Lavoryk <olhalavoryk@gmail.com>
2024-08-23 10:13:42 +0200

    Changed LCG stack to Alma 9 for portal machines

--- utils/setup_root.sh
+++ utils/setup_root.sh
@@ -3,7 +3,7 @@
 if [[ $(hostname) =~ portal1 || $(hostname) =~ centos7 ]]; then
     source /cvmfs/sft.cern.ch/lcg/views/LCG_102/x86_64-centos7-gcc11-opt/setup.sh
 elif [[ $(hostname) =~ bms ]]; then
-    source /cvmfs/sft.cern.ch/lcg/views/LCG_102/x86_64-centos8-gcc11-opt/setup.sh
+    source /cvmfs/sft.cern.ch/lcg/views/LCG_106/x86_64-el9-gcc13-dbg/setup.sh
 fi
 
 export PYTHONPATH=$PWD:$PYTHONPATH
