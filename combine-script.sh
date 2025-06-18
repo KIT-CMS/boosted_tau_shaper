cf620e24a354db29b37b6c7e1d1e3191f9796031
Olha Lavoryk <olhalavoryk@gmail.com>
2025-06-18 10:00:19 +0200

    Code to make prefit plots with real data

--- combine-script.sh
+++ combine-script.sh
@@ -21,6 +21,7 @@ shapes_rootfile=${shapes_output}.root
 shapes_rootfile_synced=${shapes_output_synced}_synced.root
 
 datacard_output="datacards_dm_sim_fit_${TAG}/${NTUPLETAG}-${TAG}/${ERA}_tauid"
+datacard_output_real_data="datacards_dm_sim_fit_real_data_${TAG}/${NTUPLETAG}-${TAG}/${ERA}_tauid"
 
 # print the paths to be used
 echo "KINGMAKER_BASEDIR: $KINGMAKER_BASEDIR"
@@ -83,6 +84,44 @@ if [[ $MODE == "DATACARD" ]]; then
 fi
 
 
+if [[ $MODE == "DATACARD_REALDATA" ]]; then
+    source utils/setup_cmssw.sh
+
+        for sf_cat in "${fj_softdrop_m_categories[@]}"
+    do
+        inputfile="htt_${CHANNEL}.inputs-sm-Run${ERA}${POSTFIX}.root"
+        # for category in "dm_binned"
+        $CMSSW_BASE/bin/el9_amd64_gcc12/MorphingTauID2017 \
+            --base_path=$PWD \
+            --input_folder_mt=$shapes_output_synced \
+            --input_folder_mm=$shapes_output_synced \
+            --real_data=true \
+            --classic_bbb=false \
+            --binomial_bbb=false \
+            --jetfakes=0 \
+            --embedding=1 \
+            --verbose=true \
+            --postfix=$POSTFIX \
+            --use_control_region=false \
+            --auto_rebin=true \
+            --categories=${sf_cat} \
+            --era=$datacard_era \
+            --output=$datacard_output_real_data
+        THIS_PWD=${PWD}
+        echo $THIS_PWD
+        cd output/$datacard_output_real_data/
+    
+        cd $THIS_PWD
+
+        echo "[INFO] Create Workspace for datacard"
+        combineTool.py -M T2W -i output/$datacard_output_real_data/htt_mt_${sf_cat}/ -o workspace_${sf_cat}_realdata.root --parallel 4 -m 125
+    done
+
+    exit 0
+
+fi
+
+
 if [[ $MODE == "FIT_LIMITS" ]]; then
     source utils/setup_cmssw.sh
     combineTool.py \
@@ -102,6 +141,16 @@ if [[ $MODE == "WORKSPACE_MULTCAT" ]]; then
 
 
         combineTool.py -M T2W -i output/$datacard_output/cmb \
+        -o output_multicat.root --parallel 4 -m 125
+
+
+fi
+
+if [[ $MODE == "WORKSPACE_MULTCAT_REAL_DATA" ]]; then
+   source utils/setup_cmssw.sh
+
+
+        combineTool.py -M T2W -i output/$datacard_output_real_data/cmb \
         -o output_multicat.root --parallel 4 -m 125
 
 
@@ -156,7 +205,7 @@ if [[ $MODE == "POSTFIT" ]]; then
         --setParameterRanges r=-30,30  --setParameters r=1  -t -1 \
         --robustFit=1  --X-rtd FITTER_NEVER_GIVE_UP --cminDefaultMinimizerStrategy 2 \
         --cminFallbackAlgo Minuit2,Migrad,0:0.01  --cminPreScan \
-        --parallel 16   -v2  --saveShapes --saveWithUncertainties 
+        --parallel 16   -v2  --saveShapes --saveWithUncertainties --saveNormalizations
 
         mv fitDiagnostics.Test.root $FITFILE
         mv higgsCombine.Test.FitDiagnostics.mH125.root output/$datacard_output/cmb/
@@ -177,6 +226,19 @@ if [[ $MODE == "POSTFIT_FROM_WORKSPACE" ]]; then
 
 fi
 
+
+if [[ $MODE == "PREFIT_FROM_WORKSPACE" ]]; then
+   source utils/setup_cmssw.sh
+
+
+    PostFitShapesFromWorkspace -w output/$datacard_output_real_data/cmb/output_multicat.root \
+    -d output/$datacard_output_real_data/cmb/combined.txt.cmb \
+    --output output/$datacard_output_real_data/cmb/prefit_shapes.root \
+    --mass 125
+
+
+fi
+
 if [[ $MODE == "POSTFIT_PLOT" ]]; then
    source utils/setup_cmssw.sh
 
@@ -185,7 +247,7 @@ if [[ $MODE == "POSTFIT_PLOT" ]]; then
 
         WORKSPACE=output/$datacard_output/cmb/output_multicat.root
         CATEGORY=$RESDIR
-        FILE=output/$datacard_output/cmb/postfitshape.root
+        FILE=output/$datacard_output/cmb/postfit_shapes.root
 
         # create output folder if it does not exist
         if [ ! -d "output/postfitplots_${TAG}_postfit/" ]; then
@@ -194,8 +256,34 @@ if [[ $MODE == "POSTFIT_PLOT" ]]; then
 
         echo "[INFO] Postfits plots for category $CATEGORY"
 
-        python3 plotting/prefit-postfit.py -l --era ${ERA} --input ${FILE} --channel ${CHANNEL}  --single-category $CATEGORY --categories "None" -o output/postfitplots_${TAG}_postfit --prefit
-        python3 plotting/prefit-postfit.py -l --era ${ERA} --input ${FILE} --channel ${CHANNEL}  --single-category $CATEGORY --categories "None" -o output/postfitplots_${TAG}_postfit
+        python3 plotting/prefit-postfit-v1.py -l --era ${ERA} --input ${FILE} --channel ${CHANNEL}  --single-category $CATEGORY --categories "None" -o output/postfitplots_${TAG}_postfit --prefit
+        python3 plotting/prefit-postfit-v1.py -l --era ${ERA} --input ${FILE} --channel ${CHANNEL}  --single-category $CATEGORY --categories "None" -o output/postfitplots_${TAG}_postfit
+
+    done
+
+fi
+
+
+
+if [[ $MODE == "POSTFIT_PLOT_REAL_DATA" ]]; then
+   source utils/setup_cmssw.sh
+
+            for RESDIR in "${fj_softdrop_m_categories[@]}" 
+      do
+
+        WORKSPACE=output/$datacard_output_real_data/cmb/output_multicat.root
+        CATEGORY=$RESDIR
+        FILE=output/$datacard_output_real_data/cmb/prefit_shapes.root
+
+        # create output folder if it does not exist
+        if [ ! -d "output/postfitplots_${TAG}_postfit_realdata/" ]; then
+            mkdir -p output/postfitplots_${TAG}_postfit_realdata/
+        fi
+
+        echo "[INFO] Postfits plots for category $CATEGORY"
+
+        python3 plotting/prefit-postfit-v1.py -l --era ${ERA} --input ${FILE} --channel ${CHANNEL}  --single-category $CATEGORY --categories "None" -o output/postfitplots_${TAG}_postfit_realdata --prefit
+        # python3 plotting/prefit-postfit-v1.py -l --era ${ERA} --input ${FILE} --channel ${CHANNEL}  --single-category $CATEGORY --categories "None" -o output/postfitplots_${TAG}_postfit_realdata
 
     done
 
